6bcde9b1d5c469b585a816776a5b4f0b8b86cab9
Robin Savage <macbookbro@Robins-MacBook-Pro.local>
2019-01-21 15:23:34 -0600

    Second set of changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,32 @@
  * Main module of the application.
  */
 angular
-  .module('routingQuizApp', []);
+  .module('routingQuizApp', ['ui.router']);
+  .config(['$stateProvder', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider.state
+    .state('Home', {
+      url: '/',
+      templateUrl: 'views/instructions.html'
+    });
+    .state('Red', {
+      url: '/',
+      templateUrl: 'views/bricks.html',
+      controller: 'RedBricksCtrl as brick'
+    });
+    .state('Blue', {
+      url: '/',
+      templateUrl: 'views/bricks.html',
+      controller: 'BlueBricksCtrl as brick'
+    });
+    .state('Green', {
+      url: '/',
+      templateUrl: 'views/bricks.html',
+      controller: 'GreenBricksCtrl as brick'
+    });
+    .state('Cart', {
+      url: '/',
+      templateUrl: 'views/cart.html',
+      controller: 'CartCtrl as cart'
+    });
+  }]);
